8094395dfec94da2bc739297dbae21e8b385ba0d
Lisa Orgomowski <adminocean@github.com>
2022-09-11 02:16:31 -0700

    print list

---
+++ print_lists.js
@@ -0,0 +1,37 @@
+const request = require('request');
+
+function listPerTag( tag ){
+
+    var qwer='where=(tags,like,'+tag+')'
+    var options = {
+        method: 'GET',
+        url: 'http://base.ocean.coop/api/v1/db/data/noco/p_qzsa4qzr6rzsza/entities/views/entities',
+        qs: { offset: '0', limit: '25', where:qwer },
+        headers: {
+            'xc-auth': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1AcHN5dHJvbi5jb20iLCJmaXJzdG5hbWUiOm51bGwsImxhc3RuYW1lIjpudWxsLCJpZCI6InVzX2Zpd3V1cWc4dzJoYWJvIiwicm9sZXMiOiJ1c2VyIiwidG9rZW5fdmVyc2lvbiI6IjJhZTY3ODQyMWQ1MzZkY2U3MjEzNjE4MjFmZjVmNDYwNmMyYzlmMWJjNjBhN2Y4YmE4MmJlMDg1ZmJmZWY5MThlMzcyODJhODQ2NzgzOGNmIiwiaWF0IjoxNjYyODU3MTU4LCJleHAiOjE2NjI4OTMxNTh9.TBLyeY4s__x0TcH8xRzxbDImcGcAjEWheSBluwAamqg'
+        },
+        json:true
+    };
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        //console.log(body);
+        console.log('\n', tag.toUpperCase() )
+        for ( var i in body.list ){
+            var unit = body.list[i];
+            console.log('[('+i+')]' ,unit.email ,'     ', unit.name , unit.tags );
+        }
+    });
+
+
+}
+
+
+
+
+
+
+
+
+listPerTag('scientist')
+listPerTag('media')
+listPerTag('mailinglist')
