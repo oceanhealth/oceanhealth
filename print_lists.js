e6e792d101a7e00a2cadd762a10a760f9a408e23
Lisa Orgomowski <adminocean@github.com>
2022-09-12 03:33:39 -0700

    list

--- print_lists.js
+++ print_lists.js
@@ -10,12 +10,14 @@ warptok5 = warptok1 + ''+10+1+'' + warptok3 + warptok4;
 function listPerTag( tag ){
 
     var qwer='where=(tags,like,'+tag+')'
+
+
     var options = {
         method: 'GET',
         url: 'http://base.ocean.coop/api/v1/db/data/noco/p_qzsa4qzr6rzsza/entities/views/entities',
         qs: { offset: '0', limit: '25', where:qwer },
         headers: {
-            'xc-auth': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1AcHN5dHJvbi5jb20iLCJmaXJzdG5hbWUiOm51bGwsImxhc3RuYW1lIjpudWxsLCJpZCI6InVzX2Zpd3V1cWc4dzJoYWJvIiwicm9sZXMiOiJ1c2VyIiwidG9rZW5fdmVyc2lvbiI6IjJhZTY3ODQyMWQ1MzZkY2U3MjEzNjE4MjFmZjVmNDYwNmMyYzlmMWJjNjBhN2Y4YmE4MmJlMDg1ZmJmZWY5MThlMzcyODJhODQ2NzgzOGNmIiwiaWF0IjoxNjYyODU3MTU4LCJleHAiOjE2NjI4OTMxNTh9.TBLyeY4s__x0TcH8xRzxbDImcGcAjEWheSBluwAamqg'
+            'xc-auth': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im9yZ29tZUBwcm90b25tYWlsLmNvbSIsImZpcnN0bmFtZSI6bnVsbCwibGFzdG5hbWUiOm51bGwsImlkIjoidXNfNGlpa3B5djd3cHdncmYiLCJyb2xlcyI6InVzZXIsc3VwZXIiLCJ0b2tlbl92ZXJzaW9uIjoiOWJhOTc0ZTI4N2M2MjVjZTNkNGVkMzRlNDI4NTc0YmI2ZWQ2YWViZGUxNTkwOThhMjk3OWY0OWU3YjNhMjBkNDkzYmZiZDMxNGMyYzdkNGUiLCJpYXQiOjE2NjI5Nzg1NzEsImV4cCI6MTY2MzAxNDU3MX0.iLu4xV0x0YV9hx-VzniuS1zLvqCiWYRNQ1ALhHBB3TQ'
         },
         json:true
     };
@@ -23,7 +25,7 @@ function listPerTag( tag ){
         if (error) throw new Error(error);
         
         console.log('\n', tag.toUpperCase() )
-        console.log(body);
+        //console.log(body);
         for ( var i in body.list ){
             var unit = body.list[i];
             console.log('[('+i+')]' ,unit.email ,'     ', unit.name , unit.tags );
